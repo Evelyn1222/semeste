9e366071608681245c59b05fbdb246266d450ae5
Evelyn1222 <146492295+Evelyn1222@users.noreply.github.com>
2023-09-29 15:12:17 -0600

    4 PROGRAMA

---
+++ unidad2/Program4.cs
@@ -0,0 +1,29 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace progra4
+{
+    internal class formula
+    {
+        static void Main(string[] args)
+        {
+            int Y, A, X;
+            Console.WriteLine("ingresa valor de y");
+            Y = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("ingresar valor de A");
+            A = Int32.Parse(Console.ReadLine());
+
+            X = Y + A + 3;
+            Console.WriteLine("El resultado de la formula es:" + X);
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+        
