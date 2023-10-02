c8fc5ca3be6e528c6209c2c9c5714b21446fe835
Evelyn1222 <146492295+Evelyn1222@users.noreply.github.com>
2023-10-01 23:02:06 -0600

    programa

---
+++ Program3.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace programa3
+{
+    internal class Promedio
+    {
+        static void Main(string[] args)
+        {
+            int c1, c2, c3, c4, c5, c6;
+            float promedio;
+            Console.WriteLine("escribe tu primera calificacion");
+            c1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("escribe tu segunda calificacion");
+            c2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("escribe tu tercera calificacion");
+            c3 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("escribe tu cuarta calificacion");
+            c4 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("escribe tu quinta calificacion");
+            c5 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("escribe tu sexta calificacion");
+            c6 = Convert.ToInt32(Console.ReadLine());
+            promedio = c1 + c2 + c3 + c4 + c5 + c6 / 6;
+            Console.WriteLine("Tu promedio es:" + promedio);
+
+        }
+    }
+}
