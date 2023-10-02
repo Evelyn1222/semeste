f2ba9d42b660f414a0dbf7ebb75c853f7908d3cf
Evelyn1222 <146492295+Evelyn1222@users.noreply.github.com>
2023-10-01 22:58:37 -0600

    mis algoritmos
    codigos

---
+++ Program1.cs
@@ -0,0 +1,26 @@
+﻿//comentarios
+/*comentarios de bloque:
+ *Console.writeLine es para imprimir
+ *para imprimir texto sin variable
+ *tiene que ir entre " " doble*/
+//Cada Linea debe finalizar con punto y coma ;
+using System; //paso 1 
+namespace Programa1 //aso 1
+{
+    internal class program //paso 1
+    {
+        static void Main(string[] arg)//paso 1
+        {
+            //DECLARAR VARIABLES
+            String nombre = ("evelyn hernandez avila");
+            //Imprimir nombre referenciado en una variable
+            Console.WriteLine(nombre);
+            //Cada Linea debe finalizar con ; punto y coma
+        }
+    }
+}
+
+
+
+
+
