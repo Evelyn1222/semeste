6a853d0e62608fd8f9a3f9c7ee30214fb1fb8f1f
Evelyn1222 <146492295+Evelyn1222@users.noreply.github.com>
2023-10-01 23:00:41 -0600

    mis programas

---
+++ Program2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program2
+    {
+        static void Main(string[] args)
+        {
+            int Y, A, X;
+            Console.WriteLine("ingresar valor de Y");
+            Y = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("ingresar valor de A");
+            A = Int32.Parse(Console.ReadLine());
+
+            X = Y + A + 3;
+            Console.WriteLine("El resultado de la formula es:" + X);
+
+        }
+    }
+}
