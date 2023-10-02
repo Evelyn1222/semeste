22535e42b4458ac7dfb9a8534625cb4e3a1fc88e
Evelyn1222 <146492295+Evelyn1222@users.noreply.github.com>
2023-10-01 23:08:34 -0600

    mis programas

---
+++ program10.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre = "evelyn", apellidos = "hernandez avila", completo;
+            completo = nombre + apellidos;
+            Console.WriteLine("sumar dos numeros");
+
+            int a = 50, b = 3, suma;
+            suma = a + b;
+            Console.WriteLine("* * bienvenidos * *" + completo + "* *");
+            Console.WriteLine("la operacion suma de A=" + a + "B=" + b);
+            Console.WriteLine($"tu resultado es {suma}");
+            Console.WriteLine("_GRACIAS UWU");
+        }
+    }
+}
