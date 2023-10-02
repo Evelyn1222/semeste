68b0929a7411fda2f3927da2306f12ae255a77dc
Evelyn1222 <146492295+Evelyn1222@users.noreply.github.com>
2023-10-01 23:07:51 -0600

    mis programas

---
+++ Program8.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+
+
+namespace programa8
+{
+
+    internal class program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("lado: ");
+            double lado = double.Parse(Console.ReadLine());
+            double perimetro = lado * 6;
+            Console.WriteLine($"{perimetro} cm");
+            Console.ReadKey();
+
+        }
+    }
+}
+
+
+
+
+
