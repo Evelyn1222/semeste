3b8baa6e8014172c6fa17acde7ac5c41fa31e726
Evelyn1222 <146492295+Evelyn1222@users.noreply.github.com>
2023-10-23 03:13:47 -0400

    Add files via upload

--- unidad3 c#/Program.cs
+++ unidad3 c#/Program.cs
@@ -1,55 +1,38 @@
 ﻿using System;
 
-namespace Programa3u3
+namespace Programa4u3
 {
-    class Programa3u3
+    class Programa4u3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Escribe el número del mes:");
-            int meses = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Ingresa el valor de p1: ");
+            double p1 = Convert.ToDouble(Console.ReadLine());
 
-            switch (meses)
+            Console.Write("Ingresa el valor de p2: ");
+            double p2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Ingresa el valor de p3: ");
+            double p3 = Convert.ToDouble(Console.ReadLine());
+
+            double total = p1 + p2 + p3;
+            if (total <= 1500)
             {
-                case 1:
-                    Console.WriteLine("enero");
-                    break;
-                case 2:
-                    Console.WriteLine("febrero");
-                    break;
-                case 3:
-                    Console.WriteLine("marzo");
-                    break;
-                case 4:
-                    Console.WriteLine("abril");
-                    break;
-                case 5:
-                    Console.WriteLine("mayo");
-                    break;
-                case 6:
-                    Console.WriteLine("junio");
-                    break;
-                case 7:
-                    Console.WriteLine("julio");
-                    break;
-                case 8:
-                    Console.WriteLine("agosto");
-                    break;
-                case 9:
-                    Console.WriteLine("septiembre");
-                    break;
-                case 10:
-                    Console.WriteLine("octubre");
-                    break;
-                case 11:
-                    Console.WriteLine("noviembre");
-                    break;
-                case 12:
-                    Console.WriteLine("diciembre");
-                    break;
-                default:
-                    Console.WriteLine("No existe el mes ingresado.");
-                    break;
+                total = total - (total * 0.30);
+                Console.WriteLine("Mostrar el total (30% descuento): " + total);
+            }
+            else if (total < 1500 && total >= 1000)
+            {
+                Console.WriteLine("El total (20% descuento): " + total);
+            }
+            else if (total < 1000 && total >= 700)
+            {
+                total = total - (total * 0.10);
+                Console.WriteLine("El total (10% descuento): " + total);
+            }
+            else
+            {
+                Console.WriteLine("El total (sin descuento): " + total);
             }
         }
     }
