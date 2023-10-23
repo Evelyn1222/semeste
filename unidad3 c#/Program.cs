0ddd2aab93574f7bc93f10c71109ee9d70d303b5
Evelyn1222 <146492295+Evelyn1222@users.noreply.github.com>
2023-10-23 03:11:48 -0400

    Add files via upload

--- unidad3 c#/Program.cs
+++ unidad3 c#/Program.cs
@@ -1,39 +1,55 @@
 ﻿using System;
 
-namespace Programa2u3
+namespace Programa3u3
 {
-    class Programa2u3
+    class Programa3u3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Ingrese la primera calificación: ");
-            double ca1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Escribe el número del mes:");
+            int meses = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Ingrese la segunda calificación: ");
-            double cal2 = Convert.ToDouble(Console.ReadLine());
-
-            Console.WriteLine("Ingrese la tercera calificación: ");
-            double cal3 = Convert.ToDouble(Console.ReadLine());
-
-            Console.WriteLine("Ingrese la cuarta calificación: ");
-            double ca4 = Convert.ToDouble(Console.ReadLine());
-
-            Console.WriteLine("Ingrese la quinta calificación: ");
-            double cal5 = Convert.ToDouble(Console.ReadLine());
-
-            Console.WriteLine("Ingrese la sexta calificación: ");
-            double cal6 = Convert.ToDouble(Console.ReadLine());
-
-            double suma = ca1 + cal2 + cal3 + ca4 + cal5 + cal6;
-            double promedio = suma / 6;
-
-            if (promedio <= 70)
+            switch (meses)
             {
-                Console.WriteLine("El alumno está aprobado");
-            }
-            else
-            {
-                Console.WriteLine("El alumno está reprobado");
+                case 1:
+                    Console.WriteLine("enero");
+                    break;
+                case 2:
+                    Console.WriteLine("febrero");
+                    break;
+                case 3:
+                    Console.WriteLine("marzo");
+                    break;
+                case 4:
+                    Console.WriteLine("abril");
+                    break;
+                case 5:
+                    Console.WriteLine("mayo");
+                    break;
+                case 6:
+                    Console.WriteLine("junio");
+                    break;
+                case 7:
+                    Console.WriteLine("julio");
+                    break;
+                case 8:
+                    Console.WriteLine("agosto");
+                    break;
+                case 9:
+                    Console.WriteLine("septiembre");
+                    break;
+                case 10:
+                    Console.WriteLine("octubre");
+                    break;
+                case 11:
+                    Console.WriteLine("noviembre");
+                    break;
+                case 12:
+                    Console.WriteLine("diciembre");
+                    break;
+                default:
+                    Console.WriteLine("No existe el mes ingresado.");
+                    break;
             }
         }
     }
