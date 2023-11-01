9483115fc415f6e49757968524eae0eb04671181
Evelyn1222 <146492295+Evelyn1222@users.noreply.github.com>
2023-10-31 22:28:06 -0300

    Add files via upload

---
+++ unidad 3 java/Program22U3uwu.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.program23u3uwu;
+
+/**
+ *
+ * @author LaboratorioLTI2
+ */
+public class Program23U3uwu {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
