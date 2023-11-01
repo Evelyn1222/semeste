304f39c96211278b306f944e14f1354e87862a37
Evelyn1222 <146492295+Evelyn1222@users.noreply.github.com>
2023-10-31 22:50:32 -0300

    Add files via upload

---
+++ 23unid3.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.unidad3;
+
+/**
+ *
+ * @author LaboratorioLTI2
+ */
+public class App {
+
+    public static void main(String[] args) {
+    int n= 1;
+    do{
+      
+      System.out.println(n);
+      n++;
+    
+    }while (n<=10);
+    }
+    
+   }
+    
+
