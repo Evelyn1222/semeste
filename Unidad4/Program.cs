330e64e1106e04234eb47ea689d36615857b30ef
Evelyn1222 <146492295+Evelyn1222@users.noreply.github.com>
2023-11-08 18:04:15 -0600

    Add files via upload

---
+++ Unidad4/Program.cs
@@ -0,0 +1,24 @@
+﻿
+
+using System;
+namespace U3program5
+{
+    internal class program2U4
+    {
+        static void Main(string[] args)
+        {
+            int[] C = new int[] { 70, 69, 34, 70, 78, 90, 100, 21, 69, 100 };
+            int S = 0, promedio = 0;
+            int i;
+
+            for (i = 0; i < C.Length; i++)
+            {
+
+                S += C[i];
+
+                promedio = S / C.Length;
+            }
+            Console.WriteLine("EL PROMEDIO DE LAS CALIFICACIONES ES:  " + promedio);
+        }
+    }
+}
